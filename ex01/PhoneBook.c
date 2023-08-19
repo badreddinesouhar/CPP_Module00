6e1d1b50407684b4fd569248f4dfb5a3c03889ef
Badr eddine Souhar <bsouhar@c1r3p6.1337.ma>
2023-08-19 15:30:17 +0100

    pyhton

--- ex01/PhoneBook.c
+++ ex01/PhoneBook.c
@@ -1,31 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   PhoneBook.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bsouhar <bsouhar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/19 15:29:13 by bsouhar           #+#    #+#             */
-/*   Updated: 2023/08/19 15:29:14 by bsouhar          ###   ########.fr       */
+/*   Created: 2023/08/19 15:29:17 by bsouhar           #+#    #+#             */
+/*   Updated: 2023/08/19 15:29:18 by bsouhar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-
-
-int main()
-{
-    std::string input_line;
-
-    while (std::cin)
-    {
-        getline(std::cin, input_line);
-        if (input_line == "ADD")
-            std::cout << "ASFDGAS" << std::endl;
-        else if (input_line == "SEARCH")
-            std::cout << "sedddrchii" << std::endl ;
-        else if (input_line == "EXIT")
-            return (0);
-    }
-    return (0);
-}
